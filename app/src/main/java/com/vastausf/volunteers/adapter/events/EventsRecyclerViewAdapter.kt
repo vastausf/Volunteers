6e4e84774b625eaa37fc1ef81c48b9c9c5def7c1
Nikita Kladov <nikita.kladov@simbirsoft.com>
2019-04-09 00:52:26 +0300

    Add event recycler view

--- app/src/main/java/com/vastausf/volunteers/adapter/events/EventsRecyclerViewAdapter.kt
+++ app/src/main/java/com/vastausf/volunteers/adapter/events/EventsRecyclerViewAdapter.kt
@@ -1,2 +1,67 @@
 package com.vastausf.volunteers.adapter.events
 
+import android.annotation.SuppressLint
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.vastausf.volunteers.R
+import kotlinx.android.synthetic.main.item_event.view.*
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class EventsRecyclerViewAdapter(
+    private val items: List<EventsRecyclerViewItem>,
+    private val onLikeClick: (Long) -> Unit,
+    private val onJoinClick: (Long) -> Unit,
+    private val onLinkClick: (String) -> Unit
+): RecyclerView.Adapter<EventsRecyclerViewAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(view: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(view.context).inflate(R.layout.item_event, view, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    inner class ViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        @SuppressLint("SetTextI18n")
+        fun bind(itemData: EventsRecyclerViewItem) {
+            view.apply {
+                tvItemEventTitle.text = itemData.title
+                tvItemEventPlace.text = itemData.place
+                tvItemEventDateTime.text = "${SimpleDateFormat("H:mm dd MMMM yyyy", Locale.getDefault()).format(itemData.datetime)} (${SimpleDateFormat("H:mm", Locale.getDefault()).format(itemData.duration)})"
+                tvItemEventDescription.text = itemData.description
+                tvItemEventLikeCounter.text = itemData.likeCount.toString()
+                tvItemEventJoinCounter.text = itemData.joinCount.toString()
+
+                if (itemData.liked)
+                    ibItemEventLike.setImageDrawable(context.getDrawable(R.drawable.ic_like))
+                else
+                    ibItemEventLike.setImageDrawable(context.getDrawable(R.drawable.ic_like_outline))
+
+                if (itemData.joined)
+                    ibItemEventJoin.setImageDrawable(context.getDrawable(R.drawable.ic_join))
+                else
+                    ibItemEventJoin.setImageDrawable(context.getDrawable(R.drawable.ic_join_outline))
+
+                ibItemEventLike.setOnClickListener {
+                    onLikeClick(itemData.id)
+                }
+                ibItemEventJoin.setOnClickListener {
+                    onJoinClick(itemData.id)
+                }
+                ibItemEventLink.setOnClickListener {
+                    onLinkClick(itemData.link)
+                }
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/vastausf/volunteers/presentation/ui/fragment/main/MainFragment.kt
+++ app/src/main/java/com/vastausf/volunteers/presentation/ui/fragment/main/MainFragment.kt
@@ -1,15 +1,19 @@
 package com.vastausf.volunteers.presentation.ui.fragment.main
 
 import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import com.vastausf.volunteers.R
+import com.vastausf.volunteers.adapter.events.EventsRecyclerViewAdapter
+import com.vastausf.volunteers.adapter.events.EventsRecyclerViewItem
 import com.vastausf.volunteers.di.fragment.DaggerFragmentComponent
 import com.vastausf.volunteers.presentation.ui.fragment.base.BaseFragment
-import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.android.synthetic.main.fragment_main.view.*
 import javax.inject.Inject
 
@@ -27,6 +31,68 @@ class MainFragment : BaseFragment(), MainFragmentView {
 
         view.bLogOut.setOnClickListener {
             presenter.onLogOut()
+        }
+
+        val item = EventsRecyclerViewItem(
+            1,
+            "Some title",
+            "Saransk, School 35",
+            1554757684797,
+            3600000,
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus ut massa nec vestibulum. Fusce porta varius pulvinar. In eget scelerisque ipsum. Vivamus porta lacus felis. Pellentesque sed lacinia erat. Mauris suscipit vel dolor eget bibendum. Nullam et libero eget odio molestie lobortis. In pretium, libero eget luctus ornare, ipsum metus porttitor metus, id pretium lectus mauris sed nisi...",
+            13,
+            true,
+            84,
+            true,
+            "https://www.google.ru/",
+            "https://static.addtoany.com/images/dracaena-cinnabari.jpg"
+        )
+
+        view.rvEventList.apply {
+            adapter = EventsRecyclerViewAdapter(listOf(
+                item, item, item, item, item
+            ),
+                onLikeClick = {
+                    showToast(it)
+                },
+                onJoinClick = {
+                    showToast(it)
+                },
+                onLinkClick = {
+                    showToast(it)
+                })
+            layoutManager = LinearLayoutManager(this@MainFragment.context)
+            addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+        }
+
+        view.bnvMain.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.iEvents -> {
+                    view.clEvents.visibility = View.VISIBLE
+                    view.clProfile.visibility = View.GONE
+                    view.clGroups.visibility = View.GONE
+
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.iProfile -> {
+                    view.clEvents.visibility = View.GONE
+                    view.clProfile.visibility = View.VISIBLE
+                    view.clGroups.visibility = View.GONE
+
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.iGroups -> {
+                    view.clEvents.visibility = View.GONE
+                    view.clProfile.visibility = View.GONE
+                    view.clGroups.visibility = View.VISIBLE
+
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                else -> throw IllegalAccessException("Illegal menu item")
+            }
         }
 
         return view
